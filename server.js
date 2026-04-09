6363d818f7887d06fada3b046a0f5598c4277ce4
Akash Dwivedi <akashdwivedi22102002@gmail.com>
2026-04-09 16:25:46 +0530

    fix issue

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-import app from "./src/app.js"
+import app from "./src/app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-app.listen(3000,()=>{
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(3000,()=>{
     console.log("server is running on port 3000")
 })
