0f0380ed328c5ccfab3273e618cf8e75c574d10a
Akash Dwivedi <akashdwivedi22102002@gmail.com>
2026-04-09 18:39:01 +0530

    Add socket.io server setup and event handling documentation

---
+++ readme.md
@@ -0,0 +1,22 @@
+io => server
+socket => single user 
+
+socket.io => it is event-driven(
+    {
+       - on => event ko listen karna
+       - emit => event ko fire karna
+    }
+)
+
+#learn(
+    => methods
+    
+  -  socket.emit()
+  -  socket.broadcast().emit()
+  -  io.emit()
+
+=> from 
+    "socket.io docs" { docs -> events -> listening & broadcast}
+        or 
+    "google" { chatGPT }
+)
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("new connection created")
+
+  socket.on("message",(msg)=>{
+    console.log('user fired message event')
+    console.log(msg)
+  })
 });
+
+// socket.emit()
+// socket.broadcast().emit()
+// io.emit()
 
 httpServer.listen(3000,()=>{
     console.log("server is running on port 3000")
